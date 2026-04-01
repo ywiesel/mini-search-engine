90c20f24c8c1794d2b345166df29f08787e810d3
ywiesel <ywiesel@clemson.edu>
2026-04-01 13:12:15 -0400

    Implement basic search functionality in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,21 @@
+import React, { useState } from "react";
+import SearchBar from "./SearchBar";
+import Results from "./Results";
 
+export default function App() {
+  const [results, setResults] = useState([]);
+
+  const handleSearch = async query => {
+    const res = await fetch(`http://localhost:5000/search?q=${query}`);
+    const data = await res.json();
+    setResults(data.results);
+  };
+
+  return (
+    <div>
+      <h1>Mini Search Engine</h1>
+      <SearchBar onSearch={handleSearch} />
+      <Results results={results} />
+    </div>
+  );
+}
