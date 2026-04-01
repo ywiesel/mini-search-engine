80192af3a439d78b36925c33f8c0024eb17c3004
ywiesel <ywiesel@clemson.edu>
2026-04-01 13:11:58 -0400

    Add Results component to display search results

--- frontend/src/Results.js
+++ frontend/src/Results.js
@@ -1,1 +1,15 @@
+import React from "react";
 
+export default function Results({ results }) {
+  return (
+    <ul>
+      {results.map((r, i) => (
+        <li key={i}>
+          <h3>{r.title}</h3>
+          <p>{r.snippet}</p>
+          <a href={r.url}>{r.url}</a>
+        </li>
+      ))}
+    </ul>
+  );
+}
